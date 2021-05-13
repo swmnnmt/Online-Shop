ddc8aa839165c977379db25787890837e7d8ad9d
Alireza Nematolahy <swmn_nmt@icloud.com>
2021-05-14 01:01:30 +0430

    Building a shopping cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CART_SESSION_ID = 'cart'
 
 ROOT_URLCONF = 'myshop.urls'
 
